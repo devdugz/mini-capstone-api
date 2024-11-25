8637c678548c745618ca695b77c8d40c4ab357a6
realcdugz <connorduggan30@gmail.com>
2024-11-25 12:09:04 -0600

    Add sessions create action (login)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      cookies.signed[:user_id] = { value: user.id, httponly: true }
+      render json: { email: user.email, user_id: user.id }, status: :created
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   delete "/products/:id" => "products#destroy"
 
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 
   # post "/images" => "images#create"
 
