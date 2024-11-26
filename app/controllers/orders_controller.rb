5d17bb1ca837c9287d4d2c1ce0c00a894b742b1c
realcdugz <connorduggan30@gmail.com>
2024-11-26 15:54:39 -0600

    Moved money logic into order.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,12 @@
 class OrdersController < ApplicationController
   def create
-    product = Product.find_by(id: params[:product_id])
-    subtotal = product.price * params[:quantity].to_i
-    tax = subtotal * 0.09
-    total = subtotal + tax
-
-    @order = Order.create(
+    @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: subtotal,
-      tax: tax,
-      total: total,
     )
+    @order.calculate_totals
+    @order.save
     render :show
   end
 
